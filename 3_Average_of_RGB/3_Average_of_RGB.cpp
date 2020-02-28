2b989111a45a4a522b3e49cb0a8fc354a0f1dfe2
hduWendy <576939544@qq.com>
2020-02-28 14:35:17 +0800

    初步完成

--- 3_Average_of_RGB/3_Average_of_RGB.cpp
+++ 3_Average_of_RGB/3_Average_of_RGB.cpp
@@ -2,10 +2,31 @@
 //
 
 #include "stdafx.h"
-
-
+#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
 int main()
 {
-    return 0;
+	//读取本地的一张图片，将所有RGB值取平均，并显示出来
+	cv::Mat srcMat = imread("C:/Users/lenovo/Desktop/推文/1.jpg");
+	int height = srcMat.rows; //行数
+	int width = srcMat.cols; //每行元素的总元素数量
+	for (int j = 0; j<height; j++)
+	{
+		for (int i = 0; i<width; i++)
+		{
+			//-----------------开始处理每个像素-----------------
+			uchar average = (srcMat.at<Vec3b>(j, i)[0] + srcMat.at<Vec3b>(j, i)[1] +
+				srcMat.at<Vec3b>(j, i)[2]) / 3;
+			srcMat.at<Vec3b>(j, i)[0] = srcMat.at<Vec3b>(j, i)[1] = srcMat.at<Vec3b>(j, i)[2] = average;
+
+			//-------------结束像素处理------------------------
+		} //单行处理结束
+	}
+	imshow("src", srcMat);
+	//等待用户按键
+	waitKey(0);
+	return 0;
 }
+
 
